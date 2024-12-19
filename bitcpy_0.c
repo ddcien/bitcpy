b96e7764f61a7b8ffca325f3406ec6c9c64be7ba
Qifeng Lu <luqifeng@byhx-china.com>
2024-12-19 11:12:50 +0800

    perf: improve the perormance of `bitcpy`

--- bitcpy_0.c
+++ bitcpy_0.c
@@ -7,6 +7,7 @@
 
 #define var __auto_type
 #define let var const
+
 #define min(a, b)                                                              \
     ({                                                                         \
         let _a = (a);                                                          \
@@ -16,22 +17,23 @@
 
 static inline uint8_t _get_uint8_mask(size_t offset, size_t bitlen) {
     static const uint8_t _mask_s[] = {
-        0x80,
-        0xc0,
-        0xe0,
-        0xf0,
-        0xf8,
-        0xfc,
-        0xfe,
-        0xff,
+        0x80U,
+        0xc0U,
+        0xe0U,
+        0xf0U,
+        0xf8U,
+        0xfcU,
+        0xfeU,
+        0xffU,
     };
     return _mask_s[bitlen - 1] >> offset;
 }
 
-static inline void _bitcpy_same_offset(uint8_t* dst,
-                                       uint8_t const* src,
-                                       size_t offset,
-                                       size_t bitlen) {
+static __always_inline void _bitcpy_same_offset(uint8_t* const dst,
+                                                uint8_t const* const src,
+                                                size_t offset,
+                                                size_t bitlen) {
+    size_t idx = 0;
     if (offset) {
         let _bitlen_left = 8 - offset;
         if (bitlen <= _bitlen_left) {
@@ -45,29 +47,70 @@ static inline void _bitcpy_same_offset(uint8_t* dst,
             if (!bitlen) {
                 return;
             }
-            dst++;
-            src++;
+            idx = 1;
         }
     }
 
     let bytelen = bitlen >> 3;
     if (bytelen) {
-        memcpy(dst, src, bytelen);
-        dst += bytelen;
-        src += bytelen;
+        memcpy(dst + idx, src + idx, bytelen);
+        idx += bytelen;
     }
+
     bitlen &= 7U;
     if (bitlen) {
         let src_mask = _get_uint8_mask(0, bitlen);
-        dst[0] = (dst[0] & ~src_mask) | (src[0] & src_mask);
+        dst[idx] = (dst[idx] & ~src_mask) | (src[idx] & src_mask);
     }
 }
 
-static inline void _bitcpy_not_same_offset(uint8_t* dst,
-                                           size_t dst_offset,
-                                           uint8_t const* src,
-                                           size_t src_offset,
-                                           size_t bitlen) {
+static __always_inline void _bitcpy_dst_offset_0(uint8_t* const dst,
+                                                 uint8_t const* const src,
+                                                 size_t const src_offset,
+                                                 size_t const bitlen) {
+    size_t idx = 0;
+    let u01_len = bitlen & 7U;
+    let u08_len = (bitlen >> 3) & 7U;
+    let u64_len = bitlen >> 6;
+
+    // clang-format off
+    for (size_t i = u64_len; i--; idx += 8) {
+        dst[idx + 0] = (src[idx + 0] << src_offset) | (src[idx + 1] >> (8 - src_offset));
+        dst[idx + 1] = (src[idx + 1] << src_offset) | (src[idx + 2] >> (8 - src_offset));
+        dst[idx + 2] = (src[idx + 2] << src_offset) | (src[idx + 3] >> (8 - src_offset));
+        dst[idx + 3] = (src[idx + 3] << src_offset) | (src[idx + 4] >> (8 - src_offset));
+        dst[idx + 4] = (src[idx + 4] << src_offset) | (src[idx + 5] >> (8 - src_offset));
+        dst[idx + 5] = (src[idx + 5] << src_offset) | (src[idx + 6] >> (8 - src_offset));
+        dst[idx + 6] = (src[idx + 6] << src_offset) | (src[idx + 7] >> (8 - src_offset));
+        dst[idx + 7] = (src[idx + 7] << src_offset) | (src[idx + 8] >> (8 - src_offset));
+    }
+
+    switch (u08_len) {
+        case 7: dst[idx + 6] = (src[idx + 6] << src_offset) | (src[idx + 7] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 6: dst[idx + 5] = (src[idx + 5] << src_offset) | (src[idx + 6] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 5: dst[idx + 4] = (src[idx + 4] << src_offset) | (src[idx + 5] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 4: dst[idx + 3] = (src[idx + 3] << src_offset) | (src[idx + 4] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 3: dst[idx + 2] = (src[idx + 2] << src_offset) | (src[idx + 3] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 2: dst[idx + 1] = (src[idx + 1] << src_offset) | (src[idx + 2] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 1: dst[idx + 0] = (src[idx + 0] << src_offset) | (src[idx + 1] >> (8 - src_offset)); __attribute__((fallthrough));
+        case 0: break;
+    }
+    // clang-format on
+    if (u01_len) {
+        let src_mask = _get_uint8_mask(0, u01_len);
+        idx += u08_len;
+        dst[idx] =
+            (dst[idx] & ~src_mask) |
+            (((src[idx] << src_offset) | (src[idx + 1] >> (8 - src_offset))) &
+             src_mask);
+    }
+}
+
+static __always_inline void _bitcpy_not_same_offset(uint8_t* dst,
+                                                    size_t dst_offset,
+                                                    uint8_t const* src,
+                                                    size_t src_offset,
+                                                    size_t bitlen) {
     assert(dst_offset != src_offset);
     if (dst_offset) {
         uint8_t _sdata;
@@ -95,53 +138,7 @@ static inline void _bitcpy_not_same_offset(uint8_t* dst,
         dst += dst_offset >> 3;
         dst_offset &= 7U;
     }
-
-    assert(dst_offset == 0);
-    while (bitlen >= 64) {
-        dst[0] = (src[0] << src_offset) | (src[1] >> (8 - src_offset));
-        dst[1] = (src[1] << src_offset) | (src[2] >> (8 - src_offset));
-        dst[2] = (src[2] << src_offset) | (src[3] >> (8 - src_offset));
-        dst[3] = (src[3] << src_offset) | (src[4] >> (8 - src_offset));
-        dst[4] = (src[4] << src_offset) | (src[5] >> (8 - src_offset));
-        dst[5] = (src[5] << src_offset) | (src[6] >> (8 - src_offset));
-        dst[6] = (src[6] << src_offset) | (src[7] >> (8 - src_offset));
-        dst[7] = (src[7] << src_offset) | (src[8] >> (8 - src_offset));
-        bitlen -= 64;
-        dst += 8;
-        src += 8;
-    }
-    assert(bitlen < 64);
-    if (bitlen >= 32) {
-        dst[0] = (src[0] << src_offset) | (src[1] >> (8 - src_offset));
-        dst[1] = (src[1] << src_offset) | (src[2] >> (8 - src_offset));
-        dst[2] = (src[2] << src_offset) | (src[3] >> (8 - src_offset));
-        dst[3] = (src[3] << src_offset) | (src[4] >> (8 - src_offset));
-        bitlen -= 32;
-        dst += 4;
-        src += 4;
-    }
-    assert(bitlen < 32);
-    if (bitlen >= 16) {
-        dst[0] = (src[0] << src_offset) | (src[1] >> (8 - src_offset));
-        dst[1] = (src[1] << src_offset) | (src[2] >> (8 - src_offset));
-        bitlen -= 16;
-        dst += 2;
-        src += 2;
-    }
-    assert(bitlen < 16);
-    if (bitlen >= 8) {
-        dst[0] = (src[0] << src_offset) | (src[1] >> (8 - src_offset));
-        bitlen -= 8;
-        dst += 1;
-        src += 1;
-    }
-    assert(bitlen < 8);
-    if (bitlen) {
-        let src_mask = _get_uint8_mask(0, bitlen);
-        dst[0] = (dst[0] & ~src_mask) |
-                 (((src[0] << src_offset) | (src[1] >> (8 - src_offset))) &
-                  src_mask);
-    }
+    return _bitcpy_dst_offset_0(dst, src, src_offset, bitlen);
 }
 
 void bitcpy(void* dst,
